585eaf1650079ab665c2c9517c04e6b3feb0d362
waraphon roonnapai <roonnapai.dev@gmail.com>
2019-09-26 12:53:18 +0700

    Add react project, home page file

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Homepage from './homepage.compoent';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Homepage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/homepage.compoent.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import './homepage.style.scss';
+
+export default function homepage() {
+  return (
+    <div className="homepage">
+      <h1>Welcome to my Homepage</h1>
+      <div className="directory-menu">
+        <div className="menu-item">
+          <div className="content">
+            <div className="title">HATS</div>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <div className="title">JACKETS</div>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <div className="title">SHOES</div>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <div className="title">WOMENS</div>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <div className="title">MENS</div>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
