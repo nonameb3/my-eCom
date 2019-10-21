7451e9398574776446a6ab4999cc2b1c013df48d
waraphon roonnapai <roonnapai.dev@gmail.com>
2019-10-21 12:54:37 +0700

    add config.js

---
+++ client/src/firebase/config.js
@@ -0,0 +1,11 @@
+// firebase private config Firebase SDK snippet
+export const config = {
+  apiKey: "xxxxxxxxxxxxxxx",
+  authDomain: "xxxxxxxxxxxxxxx",
+  databaseURL: "xxxxxxxxxxxxxxx",
+  projectId: "xxxxxxxxxxxxxxx",
+  storageBucket: "xxxxxxxxxxxxxxx",
+  messagingSenderId: "xxxxxxxxxxxxxxx",
+  appId: "xxxxxxxxxxxxxxx",
+  measurementId: "xxxxxxxxxxxxxxx"
+};
--- client/src/firebase/firebase.utill.js
+++ client/src/firebase/firebase.utill.js
@@ -1,18 +1,7 @@
 import * as firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
-
-// firebase private config Firebase SDK snippet
-const config = {
-  apiKey: "xxxxxxxxxxxxxxx",
-  authDomain: "xxxxxxxxxxxxxxx",
-  databaseURL: "xxxxxxxxxxxxxxx",
-  projectId: "xxxxxxxxxxxxxxx",
-  storageBucket: "xxxxxxxxxxxxxxx",
-  messagingSenderId: "xxxxxxxxxxxxxxx",
-  appId: "xxxxxxxxxxxxxxx",
-  measurementId: "xxxxxxxxxxxxxxx"
-};
+import { config } from "./config";
 
 firebase.initializeApp(config);
 
