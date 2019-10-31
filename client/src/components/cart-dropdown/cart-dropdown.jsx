6e69f5973c7b9a9ddee48d3a19b5e2bb93f121dc
waraphon roonnapai <roonnapai.dev@gmail.com>
2019-10-31 17:46:53 +0700

    fix cart-dropdown

--- client/src/components/cart-dropdown/cart-dropdown.jsx
+++ client/src/components/cart-dropdown/cart-dropdown.jsx
@@ -5,10 +5,16 @@ import { withRouter } from "react-router-dom";
 
 import CartItem from "../cart-item/cart-item.component";
 import CustomButton from "../custom-button/custom-button.component";
+import { toggleCart } from "../../redux/cart/cart.action";
 import { selecterCartItems } from "../../redux/cart/cart.selectors";
 import "./cart-dropdown.style.scss";
 
-function cartDropdown({ cartItems, history }) {
+const cartDropdown = ({ cartItems, history, dispatch }) => {
+  const onClick = () => {
+    history.push("/checkout");
+    dispatch(toggleCart());
+  };
+
   return (
     <div className="cart-dropdown">
       <div className="cart-items">
@@ -20,12 +26,10 @@ function cartDropdown({ cartItems, history }) {
           <span className="empty-text">Cart is Empty</span>
         )}
       </div>
-      <CustomButton onClick={() => history.push("/checkout")}>
-        GO TO CHECKOUT
-      </CustomButton>
+      <CustomButton onClick={onClick}>GO TO CHECKOUT</CustomButton>
     </div>
   );
-}
+};
 
 const mapStateToProps = state => {
   return { cartItems: selecterCartItems(state) };
