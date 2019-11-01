0d35de9b208e874ccb4228e0b2c46135bcfe5f0f
waraphon roonnapai <roonnapai.dev@gmail.com>
2019-11-01 15:08:15 +0700

    add redux-persist library for localstore

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import { PersistGate } from "redux-persist/es/integration/react";
 import { Provider } from "react-redux";
 
 import App from "./App";
-import store from "./redux/store";
+import { store, persistor } from "./redux/store";
 import "./index.css";
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </BrowserRouter>
   </Provider>,
   document.getElementById("root")
--- client/src/redux/root-reducer.js
+++ client/src/redux/root-reducer.js
@@ -1,9 +1,22 @@
-import { combineReducers } from 'redux';
+import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-import userReducer from './user/user.reducer';
-import cartReducer from './cart/cart.reducer';
+import userReducer from "./user/user.reducer";
+import cartReducer from "./cart/cart.reducer";
 
-export default combineReducers({
+// session store config
+const persistConfig = {
+  key: "root",
+  storage: storage,
+  whitelist: ["cart"]
+};
+
+// reducer
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer
 });
+
+// reducer with session store
+export default persistReducer(persistConfig, rootReducer);
