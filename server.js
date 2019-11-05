0383639105bbb8ee25da0f58b9cfe7b2c15a1939
waraphon roonnapai <roonnapai.dev@gmail.com>
2019-11-05 13:24:52 +0700

    add server

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+// ==== set production ====
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
+
+// ==== start server ====
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+  console.log('server start at port: 5000')
+})
