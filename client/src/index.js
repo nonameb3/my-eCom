84378fdf942cf2a456318fcda15d9a04203811fd
waraphon roonnapai <roonnapai.dev@gmail.com>
2019-12-26 14:11:58 +0700

    config pwa to app

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { PersistGate } from "redux-persist/es/integration/react";
 import { Provider } from "react-redux";
+import * as serviceWorker from './serviceWorker'
 
 import App from "./App";
 import { store, persistor } from "./redux/store";
@@ -17,3 +18,5 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root")
 );
+
+serviceWorker.register();
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const enforce = require('express-sslify');
 const cors = require("cors");
 const compression = require("compression");
 const bodyParser = require("body-parser");
@@ -10,17 +11,24 @@ const PORT = process.env.PORT || 5000;
 
 const paymentRouter = require("./route/payment");
 
-app.use(compression());
+// ==== Config service ====
+app.use(compression()); // set gzip
+app.use(cors()); // set cors
+app.use(enforce.HTTPS({ trustProtoHeader: true })); // set https
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
 
 // ==== set route ====
 app.use("/api", paymentRouter);
 
 // ==== set production ====
-if (process.env.NODE_ENV == "production") {
+if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+
+  app.get('/service-worker.js', (req, res) => {
+    res.sendFile(path.resolve(__dirname, "..", "build", "service-worker.js"))
+  });
+
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
